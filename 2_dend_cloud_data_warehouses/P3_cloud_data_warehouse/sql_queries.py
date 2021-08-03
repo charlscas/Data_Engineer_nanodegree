ea8bdcf15dec9b7c9a6de07aa9deaf288b02b686
Carlos C <carlos-casanovas@hotmail.com>
2021-08-03 13:58:28 +0200

    Create tables: Code and documentation

--- 2_dend_cloud_data_warehouses/P3_cloud_data_warehouse/sql_queries.py
+++ 2_dend_cloud_data_warehouses/P3_cloud_data_warehouse/sql_queries.py
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS staging_events (
 """)
 
 staging_songs_table_create = ("""
-CREATE TABLE IF NOT EXISTS staging_events (
+CREATE TABLE IF NOT EXISTS staging_songs (
     num_songs int NOT NULL,
     artist_id varchar NOT NULL,
     artist_latitude decimal,
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS staging_events (
 
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (
-    songplay_id SERIAL PRIMARY KEY, 
+    songplay_id INTEGER IDENTITY(0,1), 
     start_time timestamp NOT NULL REFERENCES time(start_time), 
     user_id int NOT NULL, 
     level varchar, 
@@ -122,7 +122,7 @@ staging_events_copy = ("""
     TIMEFORMAT AS 'epochmillisecs'
     TRUNCATECOLUMNS blankasnull emptyasnull
     JSON 's3://udacity-dend/log_json_path.json'
-""").format(DWH_ROLE_ARN)
+""").format(config['IAM_ROLE']['ARN'])
 
 staging_songs_copy = ("""
     COPY staging_songs
@@ -131,7 +131,7 @@ staging_songs_copy = ("""
     FORMAT AS JSON 'auto'
     COMPUPDATE OFF
     REGION 'us-west-2'
-""").format()
+""").format(config['IAM_ROLE']['ARN'])
 
 # FINAL TABLES
 
@@ -151,8 +151,32 @@ time_table_insert = ("""
 """)
 
 # QUERY LISTS
-
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+create_table_queries = [
+    staging_events_table_create, 
+    staging_songs_table_create, 
+    time_table_create, 
+    user_table_create, 
+    artist_table_create, 
+    songplay_table_create, 
+    song_table_create
+]
+drop_table_queries = [
+    staging_events_table_drop, 
+    staging_songs_table_drop, 
+    song_table_drop, 
+    songplay_table_drop, 
+    artist_table_drop, 
+    user_table_drop, 
+    time_table_drop
+]
+copy_table_queries = [
+    staging_events_copy, 
+    staging_songs_copy
+]
+insert_table_queries = [
+    songplay_table_insert, 
+    user_table_insert, 
+    song_table_insert, 
+    artist_table_insert, 
+    time_table_insert
+]
